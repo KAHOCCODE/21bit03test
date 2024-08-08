2b726694b8916df3a1a3866d659bace2b26b1ba7
ka do doka <hoangkavudo@gmail.com>
2024-08-09 01:53:32 +0700

    Add project files.

---
+++ 21bit03/Controllers/KhachHangController.cs
@@ -0,0 +1,28 @@
+﻿using _21bit03.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _21bit03.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly QlbanHangContext _context;
+
+        public KhachHangController(QlbanHangContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _context.KhachHangs
+                .Include(k => k.ThanhPhoNavigation)
+                .ToListAsync();
+
+            return View(customers); 
+        }
+
+
+
+    }
+}
---
+++ 21bit03/Data/ChiTietHoaDon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _21bit03.Data;
+
+public partial class ChiTietHoaDon
+{
+    public int MaHd { get; set; }
+
+    public int MaSp { get; set; }
+
+    public int SoLuong { get; set; }
+
+    public virtual HoaDon MaHdNavigation { get; set; } = null!;
+
+    public virtual SanPham MaSpNavigation { get; set; } = null!;
+}
